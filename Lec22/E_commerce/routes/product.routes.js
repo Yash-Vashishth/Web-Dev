01663146dc8d1754ae88577508f67f925c5e11b1
Yash-Vashishth <imyashvashishth05@gmail.com>
2025-11-01 16:27:02 +0530

    y

--- Lec22/E_commerce/routes/product.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const controller=require("../controllers")
-router.get("/products", (req, res) => {
-    
-});
-
-module.exports = router;
--- Lec22/E_commerce/schema/products/create.schema.js
+++
@@ -1,7 +0,0 @@
-const joi = require("joi");
-
-module.exports.createProductSchema = Joi.object({
-    name: Joi.string().required().trim(),
-    price: Joi.number().required(),
-    description: Joi.string().required()
-})
